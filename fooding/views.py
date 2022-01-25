1ef94bb543607c2f7c545c49bc0c1a8e1e5b2ed9
Tahmid Anam <tahmidshovon30@gmail.com>
2022-01-26 03:19:20 +0600

    homme page backkend

--- fooding/views.py
+++ fooding/views.py
@@ -1,10 +1,17 @@
+from seller.models import Restaurant
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def homePage(request):
+    restaurants = Restaurant.objects.filter(type='restaurant', )[:8]
+    homechefs = Restaurant.objects.filter(type='home', )[:8]
+    context = {
+        'restaurants' : restaurants,
+        'homechefs': homechefs,
+    }
     # landing page
-    return render(request, 'fooding/index.html')
+    return render(request, 'fooding/index.html', context)
 
 def searchPage(request):
     # Search result
